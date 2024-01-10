0a0744878014b87c10f0294456a1570938906e56
Canine <canine@lubuntu-optiplex790.home.lab>
2024-01-09 23:35:29 -0500

    ssh

---
+++ bin/ssh-agent.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+grep -q 'BIN_DIR' <(export) || export BIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> '/dev/null' && pwd)";
+
+function main() {
+
+    source "/usr/scripts/lib/configuration-utils.sh";
+    libraries;
+
+    local C;
+
+    [[ -z "${SUPER_USER}" ]] && {
+        [[ "$(id -u)" -eq 0 ]] || {
+            for C in {"doas","sudo"}; do
+                command -v "${C}" &> '/dev/null' && {
+                    declare -x SUPER_USER="${C}";
+                }
+            done
+        }
+    }
+
+    local DIRECTORY="/var/run/pid";
+    local  FILE="${DIRECTORY}/agent.pid";
+    printf "${SSH_AGENT_PID}" > "${FILE}";
+
+    [[ -d '/var/run/pid' ]] || {
+        ${SUPER_USER} mkdir -m 777 -p "${DIRECTORY}" && ${SUPER_USER} chown "${_}";
+    }
+
+    [[ -f "${FILE}" ]] || {
+        ${SUPER_USER} touch "${FILE}" && ${SUPER_USER} chmod 666 "${_}" && ${SUPER_USER} chown root:root "${_}";
+    }
+
+    local AGENT_PID="$(cat "${FILE}")";
+    
+    [[ -n "${AGENT_PID}" ]] && ps -A | awk '{if ($1 ~ /^[[:digit:]]+$/) {print $1}}' | grep -q "${AGENT_PID}" || {
+
+        for C in $(ps -A | awk '{if ($NF == "ssh-agent") {print $1}}'); do
+            ${SUPER_USER} kill "${C}";
+        done
+
+        ssh-agent "${SHELL}";
+    }
+
+    [[ -n "${1}" ]] && {
+        
+        local -a ARRAY;
+        mapfile -t ARRAY < <(awk -v host="${1}" -f "${LIB_DIR}/awk-lib/ssh-agent.awk" "${HOME}/.ssh/config");
+        if [[ "${#ARRAY[@]}" -eq 2 ]]; then
+            grep -q "$(awk '{printf $2}' <(ssh-add -L))" <<< "${ARRAY[1]}" || {
+                ssh-add "${ARRAY[0]}";
+            } 
+        else
+            awkDynamicBorders -t "Host not found" -c "No host with the name '${1}' exists within your '${HOME}/.ssh/config' file.";
+            return 1;
+        fi
+
+        /usr/bin/ssh "${1}";
+    }
+
+    return 0;
+}
+
+main "${1}";
+exit 0;
---
+++ bin/ssh-config.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function main() {
+
+    local CONFIG="${HOME}/.ssh/config";
+
+    [[ -f "${CONFIG}" && -r "${CONFIG}" && "$(awk '{print $1}' <(du completion))" -gt 0 ]] || {
+        printf "${CONFIG}: no such readable non empty file." >&2;
+        return 1;
+    }
+
+    local -a BASH_USER_NAMES;
+    local -A "BASH_USERS_HOME_DIRECTORIES=(
+        $(
+            awk -F ':' '{
+                if ($NF == "/bin/bash") {
+                    print "[\"" $1 "\"]=\"" $6 "\"";
+                }
+            }' <(getent passwd);
+        )
+    )";
+
+    for U in "${!BASH_USERS_HOME_DIRECTORIES[@]}"; do
+        BASH_USER_NAMES+=("${U}");
+    done
+
+    while :; do
+        echo -e "\n\tBash Users List\n\t---------------${BASH_USER_NAMES[@]/#/"\n\t"}";
+        printf "\n\t";
+        read -p "Select a User: " USERNAME;
+
+        for U in "${BASH_USER_NAMES[@]}"; do
+            if [[ "${USERNAME}" == "${U}" ]]; then
+                break 2;
+            fi
+        done
+
+        echo -e "\n\t'${USERNAME}' is not on the $(hostname)\n" >&2;
+    done
+
+    sed "/IdentityFile/{s/\/.*/$(
+        awk '{
+            gsub(/\//, "\\\/", $0);
+            printf $0
+        }' <<< "${BASH_USERS_HOME_DIRECTORIES["${USERNAME}"]}/.ssh/${USERNAME}_ed25519" 2> '/dev/null';
+    )/g;}" "${CONFIG}" | sed 's/\/\//\//g'
+    return 0;
+}
+
+main;
